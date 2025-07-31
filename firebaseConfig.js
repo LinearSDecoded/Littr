1665b81b41e7a210c8289f8215482b1577bc49b4
Stanislav Olsson <stanislav.olsson@sj.se>
2025-07-31 16:45:22 +0200

    Added firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth'; // optional
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB7O85U48SfUNFIcADDu5LnnJ8SiezeqBc",
+    authDomain: "littr-4658a.firebaseapp.com",
+    projectId: "littr-4658a",
+    storageBucket: "littr-4658a.firebasestorage.app",
+    messagingSenderId: "224538289020",
+    appId: "1:224538289020:web:b36a8ba68aed75f1c5baf9",
+    measurementId: "G-5XVEBZKG2N"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app); 
